addae2ffcdfa8151b9345cb8baf0fa95a9b57a78
upkeke <storepy@qq.com>
2022-04-17 17:56:57 +0800

    222,修改

---
+++ kkkkk.py
@@ -0,0 +1,30 @@
+# -- coding: utf-8 --
+# def add(n,i):
+#     return n+i
+# def wtest():
+#     for i in range(2,6):
+#         yield i
+# g=wtest()
+#
+# for i in g:
+#     g=add(1,i)
+#     print(g)
+
+x='1234'
+n=1
+for i in x:
+    if n>2:
+        x='qwe'
+    print(i)
+    n=n+1
+print(x)
+
+
+
+#for n in [1,10]:
+
+# g=(add(1,i) for i in g)
+# print(type(g))
+
+#print(list(g))
+
